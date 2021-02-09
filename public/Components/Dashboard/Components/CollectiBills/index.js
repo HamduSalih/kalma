0740ecd4da4dedeeb41ba6e2b3bc246c1b19bea6
HamduSalih <afa.hamdu@gmail.com>
2021-02-09 11:08:03 +0000

    add a new commit

---
+++ public/Components/Dashboard/Components/CollectiBills/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+export default class Collectible extends React.Component{
+    render(){
+        return(
+                <div className={styles.topCards}>
+                    <div>
+                        <h4 style={{marginBottom: '10px', fontWeight: 500, fontSize: '20px'}}>Sum of Collectibles</h4>
+                    </div>
+                    <div className={styles.infoBox}>
+                        <div className={styles.header}>
+                            <p>Your customers owe you GHS{this.props.AmountOwed}</p>
+                        </div>
+                        <div className={styles.content}>
+                            <div className={styles.contentLeft}>
+                                <h4>Received</h4>
+                                <p>GHS{this.props.Received}</p>
+                            </div>
+                            <div className={styles.contentRight}>
+                                <h4>Unpaid</h4>
+                                <p>GHS{this.props.Unpaid}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        )
+    }
+}
+
+export class Unsettled extends React.Component{
+    render(){
+        return(
+                <div className={styles.topCards}>
+                    <div>
+                        <h4 style={{marginBottom: '10px', fontWeight: 500, fontSize: '20px'}}>Sum of Collectibles</h4>
+                    </div>
+                    <div className={styles.infoBox}>
+                        <div className={styles.header}>
+                            <p>You owe GHS{this.props.AmountOwed}</p>
+                        </div>
+                        <div className={styles.content}>
+                            <div className={styles.contentLeft}>
+                                <h4>Paid</h4>
+                                <p>GHS{this.props.Paid}</p>
+                            </div>
+                            <div className={styles.contentRight}>
+                                <h4>Owing</h4>
+                                <p>GHS{this.props.Owing}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        )
+    }
+}
--- public/Components/Dashboard/index.js
+++ public/Components/Dashboard/index.js
@@ -1,51 +1,67 @@
 import React from 'react'
 import styles from './styles.module.css'
 import NavBar from '../../Components/NavBar.js'
+import { Bar } from 'react-chartjs-2'
+import Collectible, { Unsettled } from './Components/CollectiBills'
 
 const Dashboard = () => {
+    var profitChart = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [
+            {
+                label: 'Income for selected period',
+                data: ['2000', '4000', '1000', '2500', '700', '1000','2000', '4000', '1000', '2500', '700', '1000'],
+                backgroundColor: '#e4e4e4',
+                borderColor: '#e4e4e4',
+                borderWidth: 1,
+                hoverBackgroundColor: 'yellow',
+                hoverBorderColor: 'yellow',
+            },
+            {
+                label: 'Expenditure for selected period',
+                data: ['2000', '4000', '1000', '2500', '700', '1000','2000', '4000', '1000', '2500', '700', '1000'],
+                backgroundColor: '#cccccc',
+                borderColor: '#cccccc',
+                borderWidth: 1,
+                hoverBackgroundColor: '#0000ff',
+                hoverBorderColor: '0000ff#',
+            },
+        ]
+    }
+
     return(
         <div className={styles.container}>
             <NavBar />
             
             <div className={styles.dashboardContainer}>
                 <section className={styles.firstSection}>
-                    <div className={styles.topCards}>
+                    <Collectible
+                        AmountOwed = '5000'
+                        Received = '2000'
+                        Unpaid = '3000'
+                    />
+                    <Unsettled 
+                        AmountOwed = '5000'
+                        Paid = '2000'
+                        Owing = '3000'
+                    />
+                </section>
+
+                <section className={styles.firstSection}>
+                    <div className={styles.middleCard}>
                         <div>
-                            <h4 style={{marginBottom: '10px', fontWeight: 500, fontSize: '20px'}}>Sum of Collectibles</h4>
+                            <h4 style={{marginBottom: '10px', fontWeight: 500, fontSize: '20px'}}>Profit and Loss</h4>
                         </div>
                         <div className={styles.infoBox}>
                             <div className={styles.header}>
-                                <p>Your customers owe you GHS5000</p>
+                                <p>You Profit so far is GHS2000</p>
                             </div>
                             <div className={styles.content}>
-                                <div className={styles.contentLeft}>
-                                    <h4>Received</h4>
-                                    <p>GHS 2000</p>
-                                </div>
-                                <div className={styles.contentRight}>
-                                    <h4>Unpaid</h4>
-                                    <p>GHS 3000</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.topCards}>
-                        <div>
-                            <h4 style={{marginBottom: '10px', fontWeight: 500, fontSize: '20px'}}>Unsettled Bills</h4>
-                        </div>
-                        <div className={styles.infoBox}>
-                            <div className={styles.header}>
-                                <p>Your customers owe GHS5000</p>
-                            </div>
-                            <div className={styles.content}>
-                                <div className={styles.contentLeft}>
-                                    <h4>Paid</h4>
-                                    <p>GHS 2000</p>
-                                </div>
-                                <div className={styles.contentRight}>
-                                    <h4>Owing</h4>
-                                    <p>GHS 3000</p>
-                                </div>
+                                <Bar 
+                                    data={profitChart}
+                                    height={250}
+                                    options={{maintainAspectRatio : false}}
+                                />
                             </div>
                         </div>
                     </div>
